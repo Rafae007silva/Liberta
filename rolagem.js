91d37d6a094c4b03c1cd45c118766c92fabe5973
Rafae007silva <118479845+Rafae007silva@users.noreply.github.com>
2024-05-22 15:30:39 -0300

    Add files via upload
    Primeira

---
+++ rolagem.js
@@ -0,0 +1,13 @@
+window.revelar = ScrollReveal({reset:true})
+
+//topo do site
+
+revelar.reveal('.img1',{
+    duration: 2000,
+})
+revelar.reveal('.img_primeira',{
+    duration: 800,
+})
+revelar.reveal('.fotos',{
+    duration: 800,
+})
